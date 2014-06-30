0c97e258a9c54a1a93d9021415ea8d8ef8f8e7c0
a.mottola@gmail.com <a.mottola@gmail.com@198f959d-cf10-91d9-da8c-0448af74adc4>
2014-06-30 14:25:01 +0000

    PDB is not needed anymore

--- trunk/setup.py
+++ trunk/setup.py
@@ -139,7 +139,7 @@ if sys.platform == 'darwin':
 elif sys.platform == 'win32':
 	moc = '%(qt_dir)s\\bin\\moc.exe'
 	cflags = '/I"%(qt_dir)s\\include" /I"%(qt_dir)s\\include\\QtCore" /I"%(qt_dir)s\\include\\QtGui" /I"%(qt_dir)s\\include\\QtOpenGL" /I"%(qt_dir)s\\include\\QtWebKit" /I"%(qt_dir)s\\include\\QtNetwork" /Zc:wchar_t- /Z7'
-	ldflags = '/DEBUG /PDB:None /LIBPATH:"%(qt_dir)s\\lib" QtCore%(debug_prefix)s4.lib QtGui%(debug_prefix)s4.lib QtOpenGL%(debug_prefix)s4.lib QtNetwork%(debug_prefix)s4.lib QtWebKit%(debug_prefix)s4.lib user32.lib shell32.lib gdi32.lib advapi32.lib secur32.lib'
+	ldflags = '/DEBUG /LIBPATH:"%(qt_dir)s\\lib" QtCore%(debug_prefix)s4.lib QtGui%(debug_prefix)s4.lib QtOpenGL%(debug_prefix)s4.lib QtNetwork%(debug_prefix)s4.lib QtWebKit%(debug_prefix)s4.lib user32.lib shell32.lib gdi32.lib advapi32.lib secur32.lib'
 	dlls = [
 		'%(qt_dir)s\\bin\\QtCore%(debug_prefix)s4.dll',
 		'%(qt_dir)s\\bin\\QtGui%(debug_prefix)s4.dll',
