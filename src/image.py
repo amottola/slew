f12c5e80e5f92dfcb7f34464b2af80b8e7a80404
a.mottola@gmail.com <a.mottola@gmail.com@198f959d-cf10-91d9-da8c-0448af74adc4>
2014-09-23 08:36:39 +0000

    added STYLE_FIT and STYLE_SCALE styles to Image, and alignment support for positioning its bitmap

--- src/image.py
+++ src/image.py
@@ -8,8 +8,24 @@ from gdi import *
 class Image(slew.Window):
 	NAME						= 'image'
 	
+	#defs{SL_IMAGE_
+	STYLE_FIT					= 0x00010000
+	STYLE_SCALE					= 0x00020000
+	#}
+	
 	PROPERTIES = merge(slew.Window.PROPERTIES, {
 		'bitmap':				BitmapProperty(),
+		'style':				BitsProperty(merge(slew.Window.STYLE_BITS, {
+									'fit':			STYLE_FIT,
+									'scale':		STYLE_SCALE,
+								})),
+		'align':				BitsProperty({	'top':		slew.ALIGN_TOP,
+												'bottom':	slew.ALIGN_BOTTOM,
+												'left':		slew.ALIGN_LEFT,
+												'right':	slew.ALIGN_RIGHT,
+												'hcenter':	slew.ALIGN_HCENTER,
+												'vcenter':	slew.ALIGN_VCENTER,
+												'center':	slew.ALIGN_CENTER }),
 	})
 	
 	def initialize(self):
@@ -25,7 +41,14 @@ class Image(slew.Window):
 		self.__bitmap = Bitmap.ensure(bitmap)
 		self._impl.set_bitmap(self.__bitmap)
 	
+	def get_align(self):
+		return self._impl.get_align()
+	
+	def set_align(self, align):
+		self._impl.set_align(align)
+	
 	bitmap = DeprecatedDescriptor('bitmap')
+	align = DeprecatedDescriptor('align')
 
 
 
